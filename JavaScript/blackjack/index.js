ea16e409dd8b8903f122f5c779b0400bcf407e73
David <david@verticalvelocity.se>
2023-03-11 22:28:11 +0100

    Nu funkar

--- JavaScript/blackjack/index.js
+++ JavaScript/blackjack/index.js
@@ -1,93 +1,55 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-//Kort från kungen
+function getRandomCard() {
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
 
-//let gammal = 20
-//
-//if (gammal < 100){
-//    console.log("Väx till dig!")
-//} else if (gammal === 100) {
-//    console.log("Grattis!")
-//} else {
-//    console.log("Girige jävul!")
-//}
-//
-//
-////Age question
-//let age = 21;
-//let oldenough = false;
-//
-//if (age < 21) {
-//    console.log("You ain't got the age!"); 
-//} else {
-//    console.log("Welcome to the pleasuredome!");
-//    oldenough = true;
-//};
-let firstCard = 1;
-let secondCard = 11;
-let thirdCard = 6;
-let fourthCard = 5;
-let sixCard = 2;
-let cards = [firstCard, secondCard];
-/*console.log(cards[1]);
-*/
-let sum = cards[0] + cards[1];
-console.log(sum);
-let hasblack = false;
-let isAlice = true;
-let message = " ";
-let messageEl = document.getElementById("message-el");
-//Alt. sätt för att välja element är querySelector
-let sumEL = document.querySelector("#sum-el");
-let cardsEl = document.querySelector("#cards-el");
-
-function startGame(){
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
-};
+}
 
-function renderGame(){
-        cardsEl.textContent = "Cards: "   
-        for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " ";
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
     }
     
-    sumEL.textContent = "Summa: " + sum;
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
 
-   if (sum <= 20){
-       message = "Vill du dra ett till kort?";
-   } else if (sum === 21){
-       message = "Wohoom, du har Black Jack!";
-       hasblack = true;
-   } else {
-       message = "Du är tjock";
-       isAlice = false;
-   }
-   messageEl.textContent = message;
-};
 
-function newCard(){
-    let card = 6
+function newCard() {
+    let card = getRandomCard()
     sum += card
-    cards.push(thirdCard);
-    startGame()
-};
-
-//console.log(hasblack);
-//console.log(isAlice);
-
-
-
-
-
-// Write the conditional according to these rules:
-
-// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
-// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
-// else -> "You're out of the game! 😭"
-
-
-
-
-
+    cards.push(card)
+    renderGame()
+}
